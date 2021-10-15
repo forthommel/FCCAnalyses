7956ce92cde286d10567287a161a5707d85fc98e
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-10-15 14:59:35 +0200

    First skeletton of the ONNXRuntime wrapper object

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ export LD_LIBRARY_PATH=$PWD/install/lib:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=$PWD/install/include/FCCAnalyses:$ROOT_INCLUDE_PATH
 export LD_LIBRARY_PATH=`python -m awkward.config --libdir`:$LD_LIBRARY_PATH
 
+export ONNXRUNTIME_ROOT_DIR=/cvmfs/sft.cern.ch/lcg/releases/onnxruntime/1.8.0-47224/x86_64-centos7-gcc8-opt
 
