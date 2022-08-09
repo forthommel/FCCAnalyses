64561efb863ecb125ebe4ca93e06f4c6001e73b7
github-action-benchmark <github@users.noreply.github.com>
2022-08-09 14:56:54 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for da9d2f25fbdc8cc36ee528b09322e7a1f159e1f4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660057012188,
+  "lastUpdate": 1660057013953,
   "repoUrl": "https://github.com/forthommel/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -4917,6 +4917,45 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "laurent.forthomme@cern.ch",
+            "name": "Laurent Forthomme",
+            "username": "forthommel"
+          },
+          "committer": {
+            "email": "laurent.forthomme@cern.ch",
+            "name": "Laurent Forthomme",
+            "username": "forthommel"
+          },
+          "distinct": true,
+          "id": "da9d2f25fbdc8cc36ee528b09322e7a1f159e1f4",
+          "message": "Renamed test/ -> scripts/ to prepare for future unit tests/CI tests developments",
+          "timestamp": "2022-08-09T16:46:10+02:00",
+          "tree_id": "ec1d1577386a5e237ba6d9ff14690f85e0d7a4cd",
+          "url": "https://github.com/forthommel/FCCAnalyses/commit/da9d2f25fbdc8cc36ee528b09322e7a1f159e1f4"
+        },
+        "date": 1660057013261,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 6.513424687665178,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 4.864377384121655,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test",
+            "value": 6.4117466180067595,
+            "unit": "Evt/s"
           }
         ]
       }
